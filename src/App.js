4bf6a4fdb0a38df99dd7bedd71d932f48864d9e1
doliBee29 <dolores.balic@gmail.com>
2023-10-19 23:09:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import './App.css';
+import Messages from "./components/Messages";
+import Input from "./components/Input";
+
+function randomName() {
+  const adjectives = [
+    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+    "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long",
+    "late", "lingering", "bold", "little", "morning", "muddy", "old", "red",
+    "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering",
+    "withered", "wild", "black", "young", "holy", "solitary", "fragrant",
+    "aged", "snowy", "proud", "floral", "restless", "divine", "polished",
+    "ancient", "purple", "lively", "nameless"
+  ];
+  const nouns = [
+    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog",
+    "smoke", "star"
+  ];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
+}
+
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      member: {
+        username: randomName(),
+        color: randomColor(),
+      },
+    };
+
+    this.drone = new window.Scaledrone("X5jEkKSm943Xsyji", {
+      data: this.state.member,
+    });
+  }
+
+  componentDidMount() {
+    this.drone.on("open", (error) => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = { ...this.state.member };
+      member.id = this.drone.clientId;
+      this.setState({ member });
+    });
+    const room = this.drone.subscribe("observable-room");
+    room.on("data", (data, member) => {
+      const messages = this.state.messages;
+      messages.push({ member, text: data });
+      this.setState({ messages });
+    });
+  }
+
+  onSendMessage = (message) => {
+    if (message.trim() !== "") {
+      this.drone.publish({
+        room: "observable-room",
+        message,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1 className="App-logo">ChatSphere</h1>
+        </div>
+        <div className="App-cross"></div>
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+        <Input onSendMessage={this.onSendMessage} />
+      </div>
+    );
+  }
+}
+
+export default App;
